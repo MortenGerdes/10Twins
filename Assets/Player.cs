777e28452cb060c72690ba2a723c080deb58d909
Morten <mortengerdes7000@gmail.com>
2016-06-09 22:48:58 +0200

    Important Crossplatformmanager giving users the possibility to play with a joystick if they please

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,31 +2,19 @@
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	public bool tryingToGrab = false;
-	public bool isGrabbing = false;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (CrossPlatformInputManager.GetButton ("Fire3")) {
-			tryingToGrab = true;
-		} else {
-			tryingToGrab = false;
-			isGrabbing = false;
-		}
-	}
+    }
 
-	void OnCollisionEnter (Collision collision) {
-		print (collision.gameObject.tag);
-		if (tryingToGrab && collision.gameObject.tag == "Grabbable") {
-			isGrabbing = true;
-		}
-	}
-
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+        Debug.Log("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+    }
 }
